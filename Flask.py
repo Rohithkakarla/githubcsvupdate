e4fa173cc271dd695be42e4beeeeb6157c557a81
Rohithkakarla <karthik.kakarla30@gmail.com>
2021-08-07 17:27:10 +0530

    Updated the project

---
+++ Flask.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from Model import printcsv
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello World'
+
+@app.route('/print')
+def modelrun():
+    printcsv()
+    
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", debug=True)
